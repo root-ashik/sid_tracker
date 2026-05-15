663dae0027c9e0ed179e65d14932ca296a8fb70a
ASHIK KUNJUMON <137702934+root-ashik@users.noreply.github.com>
2026-05-15 14:07:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def init_sheets():
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-    credentials = ServiceAccountCredentials.from_json_keyfile_name("siddemo-key.json", scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("gcp_service_account", scope)
     client = gspread.authorize(credentials)
     spreadsheet = client.open_by_key("1Cwe5jN7iiNYV9LKReshFl_BLvcuAJ3pgMeWBWaRstCY")
     return spreadsheet.worksheet("Cus_Details"), spreadsheet.worksheet("Tracker")
